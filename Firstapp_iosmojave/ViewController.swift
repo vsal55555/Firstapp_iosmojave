3d56bff77a34b336f2062768c194cd4e20964b8e
BSAL-MAC <bsal-mac@BSAL-MACs-iMac.local>
2020-04-03 21:12:29 -0700

    Initial Commit

---
+++ Firstapp_iosmojave/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firstapp_iosmojave
+//
+//  Created by BSAL-MAC on 4/3/20.
+//  Copyright © 2020 BSAL-MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
